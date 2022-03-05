daea95e0ab1f55de733ea919acec9c042896165c
ohansFavour <fohanekwu@gmail.com>
2022-03-05 21:54:31 +0100

    improve gutter update performance

--- app/client/src/components/editorComponents/CodeEditor/index.tsx
+++ app/client/src/components/editorComponents/CodeEditor/index.tsx
@@ -136,9 +136,13 @@ export type EditorStyleProps = {
   popperZIndex?: Indices;
 };
 
-export type CodeEditorGutter = {
+export type GutterConfig = {
   line: (editorValue: string) => number | null;
   element: HTMLElement;
+};
+
+export type CodeEditorGutter = {
+  gutterConfig: GutterConfig[] | null;
   gutterId: string;
 };
 
@@ -164,7 +168,7 @@ export type EditorProps = EditorStyleProps &
     handleMouseEnter?: (event: MouseEvent) => void;
     handleMouseLeave?: () => void;
     // Custom gutter
-    customGutters?: CodeEditorGutter[];
+    customGutter?: CodeEditorGutter;
     // custom key map
     customKeyMap?: CustomKeyMap;
   };
@@ -259,10 +263,8 @@ class CodeEditor extends Component<Props, State> {
         };
       }
 
-      if (this.props.customGutters) {
-        this.props.customGutters.map((gutter) => {
-          gutters.add(gutter.gutterId);
-        });
+      if (this.props.customGutter) {
+        gutters.add(this.props.customGutter.gutterId);
       }
       options.gutters = Array.from(gutters);
 
@@ -448,14 +450,13 @@ class CodeEditor extends Component<Props, State> {
   }
 
   handleCustomGutters = (editor: CodeMirror.Editor) => {
-    if (!this.props.customGutters || !editor) return;
-    this.props.customGutters.forEach((gutter) => {
-      editor.clearGutter(gutter.gutterId);
-    });
-    this.props.customGutters.forEach((gutter) => {
+    const { customGutter } = this.props;
+    if (!customGutter || !editor) return;
+    editor.clearGutter(customGutter.gutterId);
+    customGutter.gutterConfig?.forEach((gutter) => {
       const lineNumber = gutter.line(editor.getValue());
       if (isNil(lineNumber)) return;
-      editor.setGutterMarker(lineNumber, gutter.gutterId, gutter.element);
+      editor.setGutterMarker(lineNumber, customGutter.gutterId, gutter.element);
     });
   };
 
